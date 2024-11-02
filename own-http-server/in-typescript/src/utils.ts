d8e080ab0a9fcf1ee90f55b1050b9572baa5522f
subh <harvirbuttar2917@gmail.com>
2024-11-02 14:51:29 -0700

    [Implemented Gzip compression] zlibGzipCompress will be used to compress data into gzip format in the case if client is capable to handle gzip data

--- own-http-server/in-typescript/src/utils.ts
+++ own-http-server/in-typescript/src/utils.ts
@@ -1,9 +1,27 @@
 // Byimaan
 
-import { HttpHeader, HttpRequest, Methods, valueOf } from "./types";
+import { error } from 'console';
 
 export const isString = (val:unknown) => typeof val === 'string'
 
 export const spacer = (times:number=1) => {
     return Array.from({length: times}, () => "\r\n").join("")
+};
+
+export const zlibGzipCompress = async (data:any) => {
+
+    const zlib = await import('zlib');
+    return new Promise<Error | Buffer>(
+        (res, rej) => {
+            zlib.gzip(data, (error, compressedData) => {
+                if (error){
+                    console.log(`[Utils: zlibGzipCompress] An error occurred while compressing data into gzip format ${error.message}`);
+                    rej(error)
+                } else {
+                    res(compressedData)
+                }
+            })
+        }
+    );
+    
 }
