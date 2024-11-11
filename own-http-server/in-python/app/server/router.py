9bfc59f4bd3e708f2813b408fee5038168f3db49
subh <harvirbuttar2917@gmail.com>
2024-11-11 14:39:53 -0800

    [Init Router] Added base class that would hande the route handling defined by the developer

---
+++ own-http-server/in-python/app/server/router.py
@@ -0,0 +1,54 @@
+# Byimaan
+
+import re
+from typing import Callable, Dict
+
+from app.constants.http import HTTP_METHODS
+
+class Router:
+    def __init__(self) -> None:
+        self._route_table: Dict [
+            str, # method
+            Dict [
+                str, # path-pattern
+                Dict [
+                    str, # regex
+                    Callable # fn that controls request / response 
+                ]
+            ]
+        ] = {}
+
+    def __set_route(self, method: str, path: str, handlerFn: Callable):
+
+        if method not in HTTP_METHODS:
+            raise ValueError(f"[Error] {method} is not a valid HTTP method")
+
+        if method not in self._route_table:
+            self._route_table[method] = { }
+        
+        if isinstance(self._route_table[method], dict):
+            regex = re.compile(
+                "^" # start-of-the-pattern
+                + path.replace("*", ".*") # so regex consider * as wildcard
+                + "$" # end-of-the-pattern
+            )
+
+            self._route_table[method][regex.pattern] = {
+                "regex": regex,
+                "handler": handlerFn
+            }
+
+    def get(self, path, handler):
+        self.__set_route("GET", path, handler)
+    
+    def post(self, path, handler):
+        self.__set_route("POST", path, handler)
+
+    def patch(self, path, handler):
+        self.__set_route("PATCH", path, handler)
+
+    def delete(self, path, handler):
+        self.__set_route("DELETE", path, handler)
+
+    def put(self, path, handler):
+        self.__set_route("PUT", path, handler)
